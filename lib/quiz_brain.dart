ee10d6ff65363fdb8df6c7c3c9a30724b78c5647
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-16 12:48:09 +0500

    Challanges have been solved

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -45,9 +45,7 @@ class QuizBrain {
     return _questionBank[_questionNumber].questionAnswer;
   }
 
-  bool isFinished(){
-    if(_questionNumber == _questionBank.last)
-  }
+  bool isFinished() {}
 
   //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
 
